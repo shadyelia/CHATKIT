214de9ca248c9e5fc68daf9e8cb4449f74a04dbc
shadyelia <shady.elia@orca-tech.net>
2019-10-13 12:15:37 +0200

    add node server and angular app

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to CHATKIT!');
+    expect(page.getTitleText()).toEqual('Welcome to client!');
   });
 
   afterEach(async () => {
