ab70019c460412fc498609c52b177528df4e0908
shadyelia <shady.elia@orca-tech.net>
2019-10-14 09:10:21 +0200

    finsih private room

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,6 +15,12 @@ export class AppComponent {
   roomUsers = [];
   userRooms = [];
   newMessage = "";
+  newRoom = {
+    name: "",
+    isPrivate: false
+  };
+  joinableRooms = [];
+  newUser = "";
 
   connectToRoom(id) {
     this.messages = [];
@@ -65,18 +71,85 @@ export class AppComponent {
         const tokenProvider = new Chatkit.TokenProvider({
           url: "http://localhost:5200/authenticate"
         });
-
         const chatManager = new Chatkit.ChatManager({
           instanceLocator: "v1:us1:cd98d4cf-e8dc-4541-a478-d80d7db00f64",
           userId,
           tokenProvider
         });
-
-        return chatManager.connect().then(currentUser => {
-          this.currentUser = currentUser;
-          this.connectToRoom("064f2d6f-fa63-4bcd-b033-fc8e3ac81594");
-        });
+        return chatManager
+          .connect({
+            onAddedToRoom: room => {
+              this.userRooms.push(room);
+              this.getJoinableRooms();
+            }
+          })
+          .then(currentUser => {
+            this.currentUser = currentUser;
+            this.connectToRoom("064f2d6f-fa63-4bcd-b033-fc8e3ac81594");
+            this.getJoinableRooms();
+          });
       })
       .catch(error => console.error(error));
+  }
+
+  createRoom() {
+    const {
+      newRoom: { name, isPrivate },
+      currentUser
+    } = this;
+
+    if (name.trim() === "") return;
+
+    currentUser
+      .createRoom({
+        name,
+        private: isPrivate
+      })
+      .then(room => {
+        this.connectToRoom(room.id);
+        this.newRoom = {
+          name: "",
+          isPrivate: false
+        };
+      })
+      .catch(err => {
+        console.log(`Error creating room ${err}`);
+      });
+  }
+
+  getJoinableRooms() {
+    const { currentUser } = this;
+    currentUser
+      .getJoinableRooms()
+      .then(rooms => {
+        this.joinableRooms = rooms;
+      })
+      .catch(err => {
+        console.log(`Error getting joinable rooms: ${err}`);
+      });
+  }
+
+  joinRoom(id) {
+    const { currentUser } = this;
+    currentUser.joinRoom({ roomId: id }).catch(err => {
+      console.log(`Error joining room ${id}: ${err}`);
+    });
+  }
+
+  addUserToRoom() {
+    const { newUser, currentUser, currentRoom } = this;
+    currentUser
+      .addUserToRoom({
+        userId: newUser,
+        roomId: currentRoom.id
+      })
+      .then(currentRoom => {
+        this.roomUsers = currentRoom.users;
+      })
+      .catch(err => {
+        console.log(`Error adding user: ${err}`);
+      });
+
+    this.newUser = "";
   }
 }
